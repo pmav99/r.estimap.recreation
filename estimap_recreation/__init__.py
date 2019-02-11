01cf7ac112f91d6ec513f80f8f3f0f1f8417918b
Nikos Alexandris <nik@nikosalexandris.net>
2019-02-11 11:20:05 +0100

    Restructure tree towards a python package

---
+++ estimap_recreation/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@author Nikos Alexandris |
+"""
+
+__all__ = ['colors', 'constants']
+
+# from estimap-recreation.colors import *
+# from estimap-recreation.constants import *
+
+version = "0.3"
